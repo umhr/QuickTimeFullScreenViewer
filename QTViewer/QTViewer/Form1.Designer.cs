1999078df1560543a3016c4bef54559dca442d18
Hiroshi Umehara <Hiroshi Umehara>
2013-09-07 20:07:00 +0900

    add

---
+++ QTViewer/QTViewer/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace QTViewer
+{
+    partial class QTFullScreenViewer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QTFullScreenViewer));
+            this.axQTControl1 = new AxQTOControlLib.AxQTControl();
+            ((System.ComponentModel.ISupportInitialize)(this.axQTControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axQTControl1
+            // 
+            this.axQTControl1.Enabled = true;
+            this.axQTControl1.Location = new System.Drawing.Point(0, 0);
+            this.axQTControl1.Name = "axQTControl1";
+            this.axQTControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axQTControl1.OcxState")));
+            this.axQTControl1.Size = new System.Drawing.Size(192, 192);
+            this.axQTControl1.TabIndex = 1;
+            // 
+            // QTFullScreenViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.axQTControl1);
+            this.Name = "QTFullScreenViewer";
+            this.Text = "QTFullScreenViewer";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.axQTControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxQTOControlLib.AxQTControl axQTControl1;
+
+    }
+}
+
---
+++ QTViewer/QTViewer/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace QTViewer
+{
+    public partial class QTFullScreenViewer : Form
+    {
+        public QTFullScreenViewer()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
+            // ◆モニタサイズを取得（複数の場合も対応）
+            // モニタサイズ保持用の構造体を生成
+            Rectangle screenRectangle = new Rectangle(0, 0, 0, 0);
+
+            // ディスプレイの解像度を取得するには？
+            // http://www.atmarkit.co.jp/fdotnet/dotnettips/003screen/screen.html
+            foreach (Screen s in Screen.AllScreens)
+            {
+                //Console.WriteLine(s.Bounds);
+                screenRectangle.X = Math.Min(screenRectangle.X, s.Bounds.X);
+                screenRectangle.Y = Math.Min(screenRectangle.Y, s.Bounds.Y);
+
+                screenRectangle.Width = Math.Max(screenRectangle.Width, s.Bounds.Right);
+                screenRectangle.Height = Math.Max(screenRectangle.Height, s.Bounds.Bottom);
+            }
+            //Console.WriteLine(screenRectangle);
+
+            // ウィンドウとQTを配置するエリアの位置やサイズを指定
+            axQTControl1.Top = 0;
+            axQTControl1.Left = 0;
+            this.Top = screenRectangle.Top;
+            this.Left = screenRectangle.Left;
+            this.Width = axQTControl1.Width = screenRectangle.Width - screenRectangle.X;
+            this.Height = axQTControl1.Height = screenRectangle.Height - screenRectangle.Y;
+
+            string[] cmds = System.Environment.GetCommandLineArgs();
+            string path = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "movie.mp4";
+
+
+            if (cmds.Length > 1)
+            {
+                //ドロップされたファイルのパスをすべて表示
+                for (int i = 1; i < cmds.Length; i++)
+                {
+                    // Console.WriteLine(cmds[i]);
+                    path = cmds[i];
+                }
+            }
+
+            axQTControl1.URL = path;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            axQTControl1.Width = this.Width;
+            axQTControl1.Height = this.Height;
+        }
+
+    }
+}
